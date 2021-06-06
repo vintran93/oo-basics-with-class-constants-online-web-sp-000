ab62a1cba0d067a30816430530158dd7eb91aa34
Vincent Tran <vincentuong@yahoo.com>
2021-06-06 05:38:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-BRAND = []
+BRANDs = []
 
 
   def initialize(brand)
@@ -17,7 +17,7 @@ BRAND = []
 
   def brand=(brand)
       @brand = brand
-      BRAND << brand
+      BRANDS << brand
   end
   
 end 
